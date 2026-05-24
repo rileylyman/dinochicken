6839be719cb6df975c2d44ea278b3e9be897b13b
ICE <asterhimalaicus@gmail.com>
2026-05-24 09:47:01 -0400

    fix

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -5,26 +5,26 @@ enum Currency {POOP, DNA, FOOD,}
 var curDinoType:DinoType = DinoType.eoraptor
 
 var amountDNA: float = 0.0
-var amountPoop: float = 0.0
-var amountFood: float = 0.0
+var amountPOOP: float = 0.0
+var amountFOOD: float = 0.0
 
 func Change_Currency_Amount(c, a: float):
 	match c:
 		Currency.POOP:
-			amountPoop = snapped(amountPoop + a, 0.01)
+			amountPOOP = snapped(amountPOOP + a, 0.01)
 		Currency.DNA:
 			amountDNA = snapped(amountDNA + a, 0.01)
 		Currency.FOOD:
-			amountFood = snapped(amountFood + a, 0.01)
+			amountFOOD = snapped(amountFOOD + a, 0.01)
 
 func Get_Currency_Amount(c):
 	match c:
 		Currency.POOP:
-			return snapped(amountPoop, 0.01)
+			return snapped(amountPOOP, 0.01)
 		Currency.DNA:
 			return snapped(amountDNA, 0.01)
 		Currency.FOOD:
-			return snapped(amountFood, 0.01)
+			return snapped(amountFOOD, 0.01)
 	return 0.0
 	
 func Get_Dino_Name_String(type:DinoType):
@@ -44,9 +44,9 @@ var speedDNA = 0.5
 var speedFOOD = 0.4
 var speedPOOP = 0.3
 
-var tmpAmountDNA = 0.0
-var tmpAmountFOOD = 0.0
-var tmpAmountPOOP = 0.0
+var tmpAmountDNA = amountDNA
+var tmpAmountFOOD = amountFOOD
+var tmpAmountPOOP = amountPOOP
 
 func _process(delta: float) -> void:
 	tmpAmountDNA += speedDNA * delta
@@ -56,6 +56,6 @@ func _process(delta: float) -> void:
 	timer += delta
 	if timer >= 0.3:
 		amountDNA = tmpAmountDNA
-		amountFood = tmpAmountFOOD
-		amountPoop = tmpAmountPOOP
-		timer = 0.0 # 计时器清零，等待下一个 0.3 秒
+		amountFOOD = tmpAmountFOOD
+		amountPOOP = tmpAmountPOOP
+		timer = 0.0
